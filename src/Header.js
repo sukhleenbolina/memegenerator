f2a8ee4f16d2564e68ce880b5799cd46b6442a19
Sukhleen <sbolina@eagles.ewu.edu>
2019-12-08 19:15:18 -0800

    Initial Commit (running)

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="\img\MemeCat.png" 
+                alt="Cat?"
+            />
+            <p>Random Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
